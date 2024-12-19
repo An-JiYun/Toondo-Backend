1067dc132e2ea43b04c3f81c4f3f90f11039a9c4
An-JiYun <81212388+An-JiYun@users.noreply.github.com>
2024-12-19 15:47:12 +0900

    투두 일부

--- src/main/java/com/project/toondo/dto/DdayToDoRequest.java
+++ src/main/java/com/project/toondo/dto/DdayToDoRequest.java
@@ -1,11 +1,16 @@
 package com.project.toondo.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 import java.time.LocalDate;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 @Data
+@Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class DdayToDoRequest {
